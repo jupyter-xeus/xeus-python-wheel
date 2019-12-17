7fdb855b2c2b7f1eb5b3dbd1a6ea8b5a53f0cfe8
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-12-17 22:34:35 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+
+try:
+    from skbuild import setup
+    from skbuild.exceptions import SKBuildError
+    from skbuild.cmaker import get_cmake_version
+    from packaging.version import LegacyVersion
+
+    setup_requires = []
+    try:
+        if LegacyVersion(get_cmake_version()) < LegacyVersion("3.15"):
+            setup_requires.append('cmake')
+    except SKBuildError:
+        setup_requires.append('cmake')
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name="xeus-python",
+    version="0.6.8",
+    description='A wheel for xeus-python',
+    author='Sylvain Corlay',
+    license='',
+    packages=[],
+    setup_requires=setup_requires,
+    #cmake=['-DHELLO_BUILD_TESTING:BOOL=TRUE',]
+)
