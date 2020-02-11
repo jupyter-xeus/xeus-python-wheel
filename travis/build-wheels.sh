82dc6d36b30e73feaa82556ca30223044b82e7d5
Johan Mabille <johan.mabille@gmail.com>
2020-02-11 09:53:50 +0100

    Skip python 3.4

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,6 +4,8 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
-    "${PYBIN}/pip" install -r io/dev-requirements.txt
-    "${PYBIN}/python" /io/setup.py bdist_wheel
+    if [ "${PYBIN}" != "/opt/python/cp34-cp34m/bin" ]; then
+        "${PYBIN}/pip" install -r /io/dev-requirements.txt
+        "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    fi
 done
