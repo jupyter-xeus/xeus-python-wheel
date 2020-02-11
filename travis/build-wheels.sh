b9e9d0905a0d187b102deda34d1d841f9e62cf95
Johan Mabille <johan.mabille@gmail.com>
2020-02-11 09:20:04 +0100

    Removed python 2.7

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/cp3*/bin; do
     "${PYBIN}/pip" install -r io/dev-requirements.txt
     "${PYBIN}/python" /io/setup.py bdist_wheel
 done
