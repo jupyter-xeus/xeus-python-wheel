0de559142bae12a6be9ab99b324d1522d8ed2eaa
Johan Mabille <johan.mabille@gmail.com>
2020-02-11 11:02:41 +0100

    Upgraded to manylinux2010

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,7 +5,9 @@ set -e -x
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
     if [ "${PYBIN}" != "/opt/python/cp34-cp34m/bin" ]; then
-        "${PYBIN}/pip" install -r /io/dev-requirements.txt
+        #"${PYBIN}/pip" install -r /io/dev-requirements.txt
+        "${PYBIN}/pip" install scikit-build 
+        "${PYBIN}/pip" install cmake
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     fi
 done
