aaec4e2c5bbd1cc41b3c3c3e7ee6643915553e99
Johan Mabille <johan.mabille@gmail.com>
2020-02-18 17:30:09 +0100

    Defined XEUS_PYTHON_INSTALL_PREFIX

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -15,8 +15,15 @@ done
 for PYBIN in /opt/python/cp3*/bin; do
     if [ "${PYBIN}" == "/opt/python/cp35-cp35m/bin" ]; then
         "${PYBIN}/pip" install --verbose xeus-python --no-index -f /io/wheelhouse
-        ls -al /io/
+        find /opt/ -name "libxeus-python.so"
+        export PATH_BU=$PATH
+        export LD_LIBRARY_PATH_BU=$LD_LIBRARY_PATH
+        export PATH=$PATH:${PYBIN}
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.5.9/lib
+        ldd ${PYBIN}/xpython
         (cd /io/test; "${PYBIN}/pytest" . -v)
+        export PATH=$PATH_BU
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_BU
     fi
 done
 
