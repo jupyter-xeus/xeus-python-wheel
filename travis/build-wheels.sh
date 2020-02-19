fea70cefa687f69c1da08e41c58cd1ce15ce6c0f
Johan Mabille <johan.mabille@gmail.com>
2020-02-19 21:06:14 +0100

    Build for python 3.6, 3.7 and 3.8

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
-    if [ "${PYBIN}" == "/opt/python/cp35-cp35m/bin" ]; then
+    if [ "${PYBIN}" != "/opt/python/cp34-cp34m/bin" ]; then
         "${PYBIN}/pip" install scikit-build 
         "${PYBIN}/pip" install -r /io/dev-requirements.txt
         "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
@@ -13,17 +13,23 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/cp3*/bin; do
-    if [ "${PYBIN}" == "/opt/python/cp35-cp35m/bin" ]; then
-        "${PYBIN}/pip" install --verbose xeus-python --no-index -f /io/wheelhouse
-        find /opt/ -name "libxeus-python.so"
-        export PATH_BU=$PATH
-        export LD_LIBRARY_PATH_BU=$LD_LIBRARY_PATH
-        export PATH=$PATH:${PYBIN}
+    export LD_LIBRARY_PATH_BU=$LD_LIBRARY_PATH
+    export PATH_BU=$PATH
+    export PATH=$PATH:${PYBIN}
+    if [ "${PYBIN}" == "/opt/python/cp34-cp34m/bin" ]; then
+        continue
+    elif [ "${PYBIN}" == "/opt/python/cp35-cp35m/bin" ]; then
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.5.9/lib
-        ldd ${PYBIN}/xpython
-        (cd /io/test; "${PYBIN}/pytest" . -v)
-        export PATH=$PATH_BU
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_BU
+    elif [ "${PYBIN}" == "/opt/python/cp36-cp36m/bin" ]; then
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.6.10/lib
+    elif [ "${PYBIN}" == "/opt/python/cp37-cp37m/bin" ]; then
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.7.6/lib
+    else
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.8.1/lib
     fi
+    "${PYBIN}/pip" install xeus-python --no-index -f /io/wheelhouse
+    (cd /io/test; "${PYBIN}/pytest" . -v)
+    export PATH=$PATH_BU
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_BU
 done
 
