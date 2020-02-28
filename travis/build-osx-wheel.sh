8fa24e7a4c46c698c29358ed3204f0eb09ad9f8d
Johan Mabille <johan.mabille@gmail.com>
2020-02-28 18:27:56 +0100

    Added installation and test of the wheel on OSX

--- travis/build-osx-wheel.sh
+++ travis/build-osx-wheel.sh
@@ -6,6 +6,10 @@ wget "http://repo.continuum.io/miniconda/Miniconda3-$MINCONDA_VERSION-$MINCONDA_
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
-conda install cmake scikit-build -c conda-forge
+conda install cmake scikit-build pytest jupyter_kernel_test -c conda-forge
 
 python setup.py bdist_wheel
+
+pip install dist/xeus_python-0.6.8-cp37-cp37m-macosx_10_13_x86_64.whl
+
+pytest test/ -v
