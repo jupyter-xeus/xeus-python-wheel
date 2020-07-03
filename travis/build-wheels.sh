5b2a6a89dcbc6668108916e3ad2786b404c5ba7e
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-07-04 00:15:04 +0200

    Drop Python 3.5 on Linux

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 # Compile wheels
 echo Compiling wheels:
 for PYBIN in /opt/python/cp3*/bin; do
-    if [ "${PYBIN}" != "/opt/python/cp34-cp34m/bin" ]; then
+    if [ "${PYBIN}" != "/opt/python/cp34-cp34m/bin" ] && [ "${PYBIN}" != "/opt/python/cp35-cp35m/bin" ]; then
         "${PYBIN}/pip" install -r /io/dev-requirements.txt
         "${PYBIN}/pip" wheel /io/ -w wheels/
         # "${PYBIN}/pip" wheel /io/ --verbose -w /io/wheelhouse/
@@ -32,10 +32,8 @@ for PYBIN in /opt/python/cp3*/bin; do
     export LD_LIBRARY_PATH_BU=$LD_LIBRARY_PATH
     export PATH_BU=$PATH
     export PATH=$PATH:${PYBIN}
-    if [ "${PYBIN}" == "/opt/python/cp34-cp34m/bin" ]; then
+    if [ "${PYBIN}" == "/opt/python/cp34-cp34m/bin" ] || [ "${PYBIN}" == "/opt/python/cp35-cp35m/bin" ]; then
         continue
-    elif [ "${PYBIN}" == "/opt/python/cp35-cp35m/bin" ]; then
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.5.9/lib
     elif [ "${PYBIN}" == "/opt/python/cp36-cp36m/bin" ]; then
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/_internal/cpython-3.6.10/lib
     elif [ "${PYBIN}" == "/opt/python/cp37-cp37m/bin" ]; then
