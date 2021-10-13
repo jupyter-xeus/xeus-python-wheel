dea57f7b8b26d4515e9c0bc957bb2dd42e5dd153
Duc Trung LE <leductrungxf@gmail.com>
2021-10-13 13:44:36 +0200

    Pass all arguments to _xpython_launch

--- xpython_launcher.py
+++ xpython_launcher.py
@@ -1,10 +1,6 @@
 if __name__ == '__main__':
-    def _xpython_get_connection_filename():
-        import argparse
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-f', help='Jupyter kernel connection filename')
-        args, unknown = parser.parse_known_args()
-        return args.f
-
+    import sys
     from xpython import launch as _xpython_launch
-    _xpython_launch(_xpython_get_connection_filename() or '')
+    args_list = sys.argv[:]
+    sys.argv = sys.argv[:1]  # Remove unnecessary arguments
+    _xpython_launch(args_list)
