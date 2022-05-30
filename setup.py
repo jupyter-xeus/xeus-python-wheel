d5098976cbca0fecaae67e4dad58a3222440de5c
martinRenou <martin.renou@gmail.com>
2022-05-30 16:16:08 +0200

    Release 0.13.8

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     install_requires=[
         'pygments>=2.3.1,<3',
         'debugpy>=1.1.0',
-        'xeus-python-shell >=0.1.5,<0.3'
+        'xeus-python-shell >=0.3.0,<0.4'
     ],
     setup_requires=setup_requires,
     cmake_args=['-DCMAKE_INSTALL_LIBDIR=lib', '-DPYTHON_EXECUTABLE:FILEPATH=' + python_path],
