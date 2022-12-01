d28ecfe28580321caf5e0022f100284221abbb56
Steven Silvester <steven.silvester@ieee.org>
2022-12-01 05:35:00 -0600

    try with explicit target

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 try:
@@ -60,6 +61,8 @@ def cmake_process_manifest_hook(cmake_manifest):
     return cmake_manifest
 
 
+openssl_config = os.environ.get('OPENSSL_CONFIG_COMMAND', './config')
+
 setup(
     name="xeus-python",
     version=xeus_version,
@@ -73,6 +76,6 @@ setup(
         'xeus-python-shell[ipython]>=0.5,<0.6'
     ],
     setup_requires=setup_requires,
-    cmake_args=['-DCMAKE_INSTALL_LIBDIR=lib', '-DPYTHON_EXECUTABLE:FILEPATH=' + python_path],
+    cmake_args=['-DCMAKE_INSTALL_LIBDIR=lib', '-DPYTHON_EXECUTABLE:FILEPATH=' + python_path, f'-DOPENSSL_CONFIG:INTERNAL={openssl_config}'],
     cmake_process_manifest_hook=cmake_process_manifest_hook
 )
